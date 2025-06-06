e0308072d76453489281822d60c609ca9576df83
ranjit <ranjitdas2048@gmail.com>
2025-06-06 09:46:47 +0530

    last commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
-import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import back from "../public/background.jpg"
 export default function Home() {
   return (
     <div className="">
-     
-     <Button></Button>
+      <div className="bg-[url(../public/background.png)] h-[90vh] bg-cover bg-blend-overlay bg-cyan-100/20 bg-no-repeat w-[100vw]  ">
+      <div className="px-10 py-5 z-2">
+        <p className="font-medium text-xl">Revi</p>
+      </div>
+      </div>
+      
     </div>
   );
 }
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-blue-900 text-primary-foreground shadow-xs hover:bg-primary/90",
+          "bg-[#7fd7df] text-primary-foreground shadow-xs hover:bg-[#7fd7df]/90",
         destructive:
           "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
