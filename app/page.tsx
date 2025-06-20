6f4dda197f8b67eb2f8e575a9b172df69d0cb427
ranjit <ranjitdas2048@gmail.com>
2025-06-20 23:07:31 +0530

    woking wiht   landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,34 @@
 "use client";
 import { Button } from "@/components/ui/button";
-
-import Image from "next/image";
-import logo from "../public/revisly.png";
-import { motion } from "framer-motion";
 import { useRouter } from "next/navigation";
-import { Sparkles } from "lucide-react";
-import srs from "../public/space.png";
+import Logo from "../public/revisly.png"
+import Image from "next/image";
+import side from "../public/side.png"
 export default function Home() {
   const navigation = useRouter();
 
   return (
-   <div>
-    
+   <div className="px-8 py-4 bg-gray-50 h-[100%]">
+    <header>
+      <div className="flex items-center justify-between gap-2 rounded-full">
+        <div className="flex items-center gap-2">
+      <Image src={Logo} width={25} height={10} alt="logo" ></Image>
+      <p className="text-[1rem] font-semibold text-neutral-900">Revisly</p>
+        </div>
+      <div className="flex gap-2 items-center">
+        <Button className="bg-gray-50 text-primary hover:cursor-pointer hover:bg-white">Login</Button>
+        <Button className="hover:cursor-pointer ">Join</Button>
+      </div>
+      </div>
+    </header>
+    <div className="grid grid-cols-2 mt-10 h-[90vh]  ">
+      <div>
+        
+      </div>
+      <div className="bg-[url(../public/side.png)] w-[100%] bg-gray-100 bg-center bg-contain bg-no-repeat">
+        {/* <Image src={side} height={100} width={500} alt="fsd"/> */}
+      </div>
+    </div>
    </div>
   );
 }
