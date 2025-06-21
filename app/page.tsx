4605c1bc0f4af329b50a25cb15f08eeee04e6fd7
ranjit <ranjitdas2048@gmail.com>
2025-06-21 19:56:53 +0530

    change minor details

--- app/page.tsx
+++ app/page.tsx
@@ -246,7 +246,7 @@ export default function Home() {
   </div>
   <p className="block mb-4 text-sm text-center text-slate-500 md:mb-0 border-t border-slate-200 mt-4 pt-4">
     Copyright © 2024&nbsp; 
-    <a href="https://material-tailwind.com/" target="_blank" rel="noreferrer">Material Tailwind</a>.
+    <a href="#" target="_blank" rel="noreferrer">Revisly</a>.
   </p>
   </footer>
       </footer>
