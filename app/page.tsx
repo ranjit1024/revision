e2b3d853e2afbf321ba4346f6583a877bddddcae
ranjit <ranjitdas2048@gmail.com>
2025-06-21 10:36:08 +0530

    done with hero secion

--- app/page.tsx
+++ app/page.tsx
@@ -8,26 +8,47 @@ export default function Home() {
   const navigation = useRouter();
 
   return (
-   <div className="px-8 py-4 bg-gray-50 h-[100%]">
+   <div className="px-10 py-4 bg-gray-50 h-[100%]">
+    <div className="">
     <header>
-      <div className="flex items-center justify-between gap-2 rounded-full">
+      <div className="flex items-center justify-between gap-2 rounded-full bg-white px-4 py-2 shadow-blue-200 shadow">
         <div className="flex items-center gap-2">
       <Image src={Logo} width={25} height={10} alt="logo" ></Image>
       <p className="text-[1rem] font-semibold text-neutral-900">Revisly</p>
         </div>
       <div className="flex gap-2 items-center">
-        <Button className="bg-gray-50 text-primary hover:cursor-pointer hover:bg-white">Login</Button>
+        <Button className="bg-white text-primary hover:cursor-pointer hover:bg-white">Login</Button>
         <Button className="hover:cursor-pointer ">Join</Button>
       </div>
       </div>
     </header>
-    <div className="grid grid-cols-2 mt-10 h-[90vh]  ">
+    <div className="grid grid-cols-2 mt-20 h-[100vh]   ">
       <div>
-        
+        <div>
+        <p className="text-[3.5rem]  font-medium">Revise scientifically.</p>
+        <p className="text-[3.5rem] -mt-2 font-medium">Remember longer </p>
+
+        <p className="text-[3.5rem] -mt-2 font-medium">Master Time</p>
+        </div>
+      <div className="mt-5 w-[80%]">
+        Automatic reminders are sent to your registered email and phone based on the date and time you set.
       </div>
-      <div className="bg-[url(../public/side.png)] w-[100%] bg-gray-100 bg-center bg-contain bg-no-repeat">
-        {/* <Image src={side} height={100} width={500} alt="fsd"/> */}
+      <div className="mt-15 flex items-center">
+        <Button className=" bg-accent-foreground hover:cursor-pointer">Get access</Button>
+        <Button className="ml-3 bg-gray-50 flex items-center text-primary hover:cursor-pointer  hover:bg-white hover:shadow">
+          <img width="20" height="48" src="https://img.icons8.com/fluency/48/google-play.png" alt="google-play"/>
+          Download app</Button>
       </div>
+      </div>
+    
+    <div className="flex justify-end items-end text-end h-[110vh] w-[100%]">
+      <div className="bg-[url(../public/side2.png)] w-[82%] h-[100%]  rounded-2xl bg-center bg-cover bg-no-repeat">
+
+    </div>
+      
+      
+      </div>
+    </div>
     </div>
    </div>
   );
