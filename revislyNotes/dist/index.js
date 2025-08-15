4377cccf87e9e2455f00b8c0087c7c4739fa9911
ranjit <ranjitdas2048@gmail.com>
2025-08-15 12:19:57 +0530

    add pdf logic

--- revislyNotes/dist/index.js
+++ revislyNotes/dist/index.js
@@ -15,10 +15,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 const redis_1 = require("@upstash/redis");
 const groq_sdk_1 = require("groq-sdk");
+const pdfkit_1 = __importDefault(require("pdfkit"));
+const fs_1 = __importDefault(require("fs"));
 dotenv_1.default.config();
 const groq = new groq_sdk_1.Groq({
     apiKey: process.env.GROQ_API_KEY
 });
+function GenerateNotesPdf() {
+    const notes = new pdfkit_1.default();
+    notes.pipe(fs_1.default.createWriteStream('notes.pdf'));
+    notes.fontSize(20).text('done', 100, 100);
+    notes.text("Welcome to KLJ GroupFounded in 1967, KLJ is not only the Largest Manufacturer and the Market Leader in Plasticizers & Polymer Compounds in South Asia, but also an outright leader in the Secondary Plasticizer - Chlorinated Paraffin segment as the Largest Manufacturer across the globe. Their Ultra-Modern Manufacturing facilities in Silvassa, Bharuch, Agra & Kutch in India, Rayong in Thailand and Mesaieed in Qatar are equipped with State-of-the-Art Technology and Equipment, producing a wide range of products to meet their Customers Requirements.", 10, 10);
+    notes.end();
+}
+GenerateNotesPdf();
 // app.use(express.json());
 const redis = redis_1.Redis.fromEnv();
 function getAiGeneratedNotes(params) {
@@ -52,4 +62,4 @@ function generateNotes() {
         }), 5000);
     });
 }
-generateNotes();
+//generaing pedf from notes
--- revislyNotes/src/index.ts
+++ revislyNotes/src/index.ts
@@ -3,18 +3,31 @@ import dotenv from "dotenv";
 import { Redis } from "@upstash/redis";
 import { Groq } from "groq-sdk";
 import { aw } from "@upstash/redis/zmscore-CgRD7oFR";
-
+import multer from "multer"
+import aws from "aws-sdk"
+import v4 from "uuid"
+import pdf, { note } from "pdfkit"
+import fs from "fs"
+import { notStrictEqual } from "assert";
 dotenv.config();
 
 const groq = new Groq({
-    apiKey:process.env.GROQ_API_KEY
+  apiKey: process.env.GROQ_API_KEY
 });
+function GenerateNotesPdf() {
+  const notes = new pdf();
+  notes.pipe(fs.createWriteStream('notes.pdf'))
 
+  notes.fontSize(20).text('done', 100, 100);
+  notes.text("Welcome to KLJ GroupFounded in 1967, KLJ is not only the Largest Manufacturer and the Market Leader in Plasticizers & Polymer Compounds in South Asia, but also an outright leader in the Secondary Plasticizer - Chlorinated Paraffin segment as the Largest Manufacturer across the globe. Their Ultra-Modern Manufacturing facilities in Silvassa, Bharuch, Agra & Kutch in India, Rayong in Thailand and Mesaieed in Qatar are equipped with State-of-the-Art Technology and Equipment, producing a wide range of products to meet their Customers Requirements.", 10,10)
+  notes.end()
+}
+GenerateNotesPdf()
 // app.use(express.json());
 const redis = Redis.fromEnv();
 
-async function getAiGeneratedNotes(params:string) {
-      const chatCompletion = await groq.chat.completions.create({
+async function getAiGeneratedNotes(params: string) {
+  const chatCompletion = await groq.chat.completions.create({
     messages: [
       {
         role: "user",
@@ -29,24 +42,25 @@ async function getAiGeneratedNotes(params:string) {
 
 async function generateNotes() {
 
-  
-  setInterval(async ()=>{
-    try{
 
-    
-    const revisionTopic = await redis.rpop("revision");
+  setInterval(async () => {
+    try {
 
-    if(revisionTopic !== null && revisionTopic.trim() !== ''){
-      console.log(`Processing: ${revisionTopic}`);
-      const notes = await getAiGeneratedNotes(`generate notes for ${revisionTopic}`);
-      console.log('Notes Generated for ', notes)
+
+      const revisionTopic = await redis.rpop("revision");
+
+      if (revisionTopic !== null && revisionTopic.trim() !== '') {
+        console.log(`Processing: ${revisionTopic}`);
+        const notes = await getAiGeneratedNotes(`generate notes for ${revisionTopic}`);
+        console.log('Notes Generated for ', notes)
+      }
     }
-  }
-  catch(err){
-    console.log('Queue processing error', err);
-  }
-  },5000)
+    catch (err) {
+      console.log('Queue processing error', err);
+    }
+  }, 5000)
 }
-generateNotes();
+
+//generaing pedf from notes
 
 
